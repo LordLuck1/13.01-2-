6ac16b23d7ddc0011bfa1d0265a484179d9cad28
LordLuck1 <94655389+LordLuck1@users.noreply.github.com>
2022-01-13 19:18:41 +0900

    Add files via upload

---
+++ Перминов 13.01/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp2.Controller;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+         Query controller; public Form1()
+        {
+            InitializeComponent();
+            controller = new Query(ConnectionString.ConnStr); //строка подключения 
+        }
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = controller.upDatePerson();
+        }
+
+        private void Button2_Click_1(object sender, EventArgs e)
+        {
+            controller.Add(textBox1.Text, textBox2.Text, int.Parse(textBox3.Text));
+        }
+
+        private void Button3_Click_1(object sender, EventArgs e)
+        {
+            controller.Delete(int.Parse(dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells["ID"].Value.ToString()));
+        }
+    }
+}
